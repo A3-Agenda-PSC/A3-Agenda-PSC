f70100d2821a0236b99c87197524ab6eecc3453e
bruuhack <130937645+bruuhack@users.noreply.github.com>
2023-06-22 20:41:54 -0300

    Add files via upload

---
+++ Contatos.java
@@ -0,0 +1,53 @@
+package br.com.agenda.model;
+
+import java.time.LocalDate;
+
+
+public class Contatos {
+	private int codigo;
+	private String nome;
+	private int idade;
+	private String telefone;
+	private String email;
+	private LocalDate dataCadastro;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+	
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+		
+	} 
+	
+}
